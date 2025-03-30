b2ea11bd8075565211d0f5fc2b0a9591f8c11d79
gakita <raulgru5@gmail.com>
2025-03-30 18:56:15 -0300

    Rotas para as telas

---
+++ app/src/main/java/projeto/integrador/routes/navController.kt
@@ -0,0 +1,21 @@
+package projeto.integrador.routes
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import projeto.integrador.data.model.Usuario
+import projeto.integrador.ui.screens.SignUpScreen
+//import projeto.integrador.ui.screens.SignInScreen
+
+@Composable
+fun NavigationSetup(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "signUp") {
+        composable("signUp") {
+            SignUpScreen(navController, Usuario("","","",""))
+        }
+//        composable("signIn") {
+//            SignInScreen()
+//        }
+    }
+}
