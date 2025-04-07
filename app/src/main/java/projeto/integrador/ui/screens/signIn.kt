2926803cbd6ceee38fc28bb11f2ff1af8f65a463
gakita <raulgru5@gmail.com>
2025-04-07 14:29:01 -0300

    Tela de Perfil

--- app/src/main/java/projeto/integrador/ui/screens/signIn.kt
+++ app/src/main/java/projeto/integrador/ui/screens/signIn.kt
@@ -2,8 +2,10 @@ package projeto.integrador.ui.screens
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,11 +15,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import projeto.integrador.utilities.funcs.validation
 import projeto.integrador.data.model.Usuario
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import projeto.integrador.routes.NavigationSetup
 
 
@@ -27,7 +31,10 @@ fun signInScreen(context:Context,modifier: Modifier, usuario: Usuario, navHostCo
     var email by remember { mutableStateOf("") }
     var senha by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
 
         //Titulo
         Text("Olá, seja bem vindo!", fontSize = 24.sp)
@@ -38,15 +45,17 @@ fun signInScreen(context:Context,modifier: Modifier, usuario: Usuario, navHostCo
             TextField(
                 value = email,
                 onValueChange = { email = it },
-                label = { Text("Email") }
+                label = { Text("Email") },
+                modifier = Modifier.padding(top = 16.dp)
             )
             TextField(
                 value = senha,
                 onValueChange = { senha = it },
                 label = { Text("Senha") },
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.padding(top = 16.dp)
             )
-            Button(onClick = {
+            Button(modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 16.dp),onClick = {
                 validation(context, email, senha) { success, message ->
                     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
                     if (success) {
