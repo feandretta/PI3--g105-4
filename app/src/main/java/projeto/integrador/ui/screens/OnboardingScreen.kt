a764bc8671077aebf65a8cb82e25a8b239264a3a
PedroOzassa <162368884+PedroOzassa@users.noreply.github.com>
2025-05-11 20:46:32 -0300

    Tela para teste

---
+++ app/src/main/java/projeto/integrador/ui/screens/OnboardingScreen.kt
@@ -0,0 +1,21 @@
+package projeto.integrador.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun OnboardingScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Bem vindo")
+        }
+    }
+}
