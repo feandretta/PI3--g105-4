1e90c47e81f2204149d3a281ecec72666ec15c70
PedroOzassa <162368884+PedroOzassa@users.noreply.github.com>
2025-05-11 23:40:39 -0300

    Tela de onboarding funcional

--- app/src/main/java/projeto/integrador/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/projeto/integrador/ui/screens/OnboardingScreen.kt
@@ -7,21 +7,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.core.content.edit
 
 @Composable
 fun OnboardingScreen(
     context: Context,
     navHostController: NavHostController
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text("Bem vindo")
-            Button(onClick = {navHostController.navigate("signUp")}) {
-                Text("Continuar")
+    val sharedPreferences = context.getSharedPreferences(
+        "projeto.integrador.PREFERENCE_FILE_KEY",
+        Context.MODE_PRIVATE
+    )
+
+    val isFirstTime = sharedPreferences.getBoolean("isFirstTime",true)
+
+    if (isFirstTime){
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("Bem vindo")
+                Button(onClick = {
+                    sharedPreferences.edit() { putBoolean("isFirstTime", false) }
+                    navHostController.navigate("signUp")
+                }) {
+                    Text("Continuar")
+                }
             }
+
         }
+    } else{
+        navHostController.navigate("signUp")
     }
+
 }
