ec59d61b75e7153ed3810ec41ce2873e3a7ccafa
PedroOzassa <162368884+PedroOzassa@users.noreply.github.com>
2025-05-21 15:06:06 -0300

    Design da tela de cadastro atualizado
    Design dos elementos da tela atualizados, mudança na logica de visibilidade de senha

--- app/src/main/java/projeto/integrador/ui/screens/signUp.kt
+++ app/src/main/java/projeto/integrador/ui/screens/signUp.kt
@@ -1,6 +1,10 @@
 package projeto.integrador.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,8 +12,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -21,7 +36,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,6 +51,8 @@ import projeto.integrador.utilities.funcs.Cadastro
 import projeto.integrador.data.model.Usuario
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import projeto.integrador.R
 
 
 fun visualizacaoSenha(visualTransformation: Boolean): Boolean {
@@ -48,9 +70,13 @@ fun SignUpScreen(navController: NavHostController, usuario: Usuario, modifier: M
     var email by remember { mutableStateOf("") }
     var senha by remember { mutableStateOf("") }
     var confirmarSenha by remember { mutableStateOf("") }
+
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
+
     var senhaVisivel by remember { mutableStateOf(false) }
+    var confirmarSenhaVisivel by remember { mutableStateOf(false) }
+    var termosAceitos by remember { mutableStateOf(false) }
 
     // Estado para controlar a exibição do diálogo de confirmação
     var showDialog by remember { mutableStateOf(false) }
@@ -71,78 +97,154 @@ fun SignUpScreen(navController: NavHostController, usuario: Usuario, modifier: M
         )
     }
 
-    Column(
+    Box(
         modifier = Modifier
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
     ) {
-        Text("Olá, seja bem vindo!", fontSize = 24.sp)
-        Text("Crie sua conta", fontSize = 24.sp)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = nome,
-            onValueChange = { nome = it },
-            label = { Text("Nome") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = senha,
-            onValueChange = { senha = it },
-            label = { Text("Senha") },
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = if (senhaVisivel) VisualTransformation.None else PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = confirmarSenha,
-            onValueChange = { confirmarSenha = it },
-            label = { Text("Confirmar Senha") },
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = if (senhaVisivel) VisualTransformation.None else PasswordVisualTransformation()
-        )
-
-        TextButton(onClick = {
-            senhaVisivel = !senhaVisivel
-        }) {
-            Text(if (senhaVisivel) "Ocultar Senha" else "Mostrar Senha")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
+                .padding(24.dp)
                 .fillMaxWidth()
-                .padding(top = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            TextButton(onClick = { navController.navigate("signIn") }) {
-                Text("Já tenho uma conta", textDecoration = TextDecoration.Underline)
-            }
-            //Vai passar os dados para a função de cadastro
-            Button(onClick = {
-                scope.launch {
-                    // Chama a função de cadastro passando context, nome, email, senha e confirmarSenha
-                    val resultado = Cadastro(context, nome, email, senha, confirmarSenha)
-                    dialogMessage = if (resultado) {
-                        "Cadastro realizado com sucesso!"
-                    } else {
-                        "Erro ao realizar o cadastro!"
-                    }
-                    showDialog = true
-                }
+            Image(
+                painter = painterResource(R.drawable.placeholder),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(150.dp)
+            )
+            Text(
+                text = "Super ID",
+                fontSize = 28.sp,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
 
-            }) {
-                Text("Cadastrar")
+            OutlinedTextField(
+                value = nome,
+                onValueChange = { nome = it },
+                label = { Text("Nome") },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Endereço de E-mail") },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+            )
+            //Implementar Secure text field?
+            OutlinedTextField(
+                value = senha,
+                onValueChange = { senha = it },
+                label = { Text("Senha") },
+                singleLine = true,
+                visualTransformation =
+                    if (senhaVisivel)
+                        VisualTransformation.None
+                    else
+                        PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                trailingIcon = {
+                    val image =
+                        if (senhaVisivel)
+                            Icons.Filled.Visibility
+                        else
+                            Icons.Filled.VisibilityOff
+                    IconButton(onClick = { senhaVisivel = !senhaVisivel }) {
+                        Icon(imageVector = image, "")//Implementar acessibilidade?
+                    }
+                }
+            )
+            //Implementar Secure text field?
+            OutlinedTextField(
+                value = confirmarSenha,
+                onValueChange = { confirmarSenha = it },
+                label = { Text("Confirmar Senha") },
+                singleLine = true,
+                visualTransformation =
+                    if (confirmarSenhaVisivel)
+                        VisualTransformation.None
+                    else
+                        PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                trailingIcon = {
+                    val image =
+                        if (confirmarSenhaVisivel)
+                            Icons.Filled.Visibility
+                        else
+                            Icons.Filled.VisibilityOff
+                    IconButton(onClick = {confirmarSenhaVisivel = !confirmarSenhaVisivel}) {
+                        Icon(imageVector = image,"")//Implementar acessibilidade?
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // TODO Implementar termos e Pol. de privacidade
+                Checkbox(
+                    checked = termosAceitos,
+                    onCheckedChange ={ termosAceitos = it }
+                )
+                Text(
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    text ="Li e estou de acordo com os Termos de Uso e Política de Privacidade"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(36.dp))
+
+            Button(
+                onClick = {
+                    scope.launch {
+                        // Chama a função de cadastro passando context, nome, email, senha e confirmarSenha
+                        val resultado = Cadastro(context, nome, email, senha, confirmarSenha)
+                        dialogMessage = if (resultado) {
+                            "Cadastro realizado com sucesso!"
+                        } else {
+                            "Erro ao realizar o cadastro!"
+                        }
+                        showDialog = true
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text("Criar conta")
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Já tem uma conta? ", color = MaterialTheme.colorScheme.onSurfaceVariant, fontSize = 14.sp)
+                Text(
+                    text = "Faça login",
+                    color = Color.Blue,
+                    fontSize = 14.sp,
+                    modifier = Modifier.clickable {
+                        navController.navigate("signIn")
+                    }
+                )
             }
         }
     }
