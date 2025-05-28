6d53cce1ae6d81ae57212ea16bab989847b501f5
Otávio Rosa Zampolli <otaviorzampolli@gmail.com>
2025-05-28 17:09:51 -0300

    return da criptografia da senha para string

--- app/src/main/java/projeto/integrador/config/encryptPassword.kt
+++ app/src/main/java/projeto/integrador/config/encryptPassword.kt
@@ -2,10 +2,10 @@ package projeto.integrador.config
 
 import java.security.MessageDigest
 
-fun encryptPassword(senha:String): ByteArray {
+fun encryptPassword(senha:String): String {
     val senhaRecebida:ByteArray = senha.toByteArray()
     val md = MessageDigest.getInstance("SHA-256")
     val senhaCripto = md.digest(senhaRecebida)
 
-    return senhaCripto
+    return senhaCripto.toString()
 }
