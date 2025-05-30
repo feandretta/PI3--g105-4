f5875d9e9e5352a7de42d6948d0e05e164ea5007
Otávio Rosa Zampolli <otaviorzampolli@gmail.com>
2025-05-30 12:21:12 -0300

    correção gerador de access token e remoção de salt
    o salt n fazia sentido nessa aplicação pois o parceiro do app ja teria um salt pra senha do usuario no sistema dela

--- app/src/main/java/projeto/integrador/config/generateAccessToken.kt
+++ app/src/main/java/projeto/integrador/config/generateAccessToken.kt
@@ -1,23 +1,11 @@
 package projeto.integrador.config
 
-import android.os.Build
-import android.util.Base64;
-import android.util.Base64.DEFAULT
-import androidx.annotation.RequiresApi
-import java.util.random.RandomGenerator
+import android.util.Base64
+import java.security.SecureRandom
 
-@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
-fun generateAccessToken() : String{
-
-    val generator : RandomGenerator = RandomGenerator.getDefault()
-
-    val stream = generator.longs(256)
-    val byteStream = stream.toString().toByteArray()
-
-    val byteEncode = Base64.encode(byteStream, DEFAULT)
-
-    return byteEncode.toString()
-
-
+fun generateAccessToken(): String {
+    val random = SecureRandom()
+    val bytes = ByteArray(32) // 256 bits = 32 bytes
+    random.nextBytes(bytes)
+    return Base64.encodeToString(bytes, Base64.NO_WRAP)
 }
-
